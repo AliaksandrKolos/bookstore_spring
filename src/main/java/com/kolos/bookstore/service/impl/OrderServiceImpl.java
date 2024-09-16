d1b670e80ade18e883040a5e1cc358cfdcc7cb4b
sanya <kolosovskii.201416@gmail.com>
2024-09-16 16:29:34 +0300

    Refactored OrderServiceImpl to use MessageManager for message retrieval, added @Transactional annotations where necessary, and updated method signatures and logic to handle long types and transaction management

--- src/main/java/com/kolos/bookstore/service/impl/OrderServiceImpl.java
+++ src/main/java/com/kolos/bookstore/service/impl/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package com.kolos.bookstore.service.impl;
 
 
-import com.kolos.bookstore.service.util.MassageManager;
 import com.kolos.bookstore.data.entity.Order;
 import com.kolos.bookstore.data.repository.OrderRepository;
 import com.kolos.bookstore.service.OrderService;
@@ -11,6 +10,10 @@ import com.kolos.bookstore.service.dto.OrderStatusUpdateDto;
 import com.kolos.bookstore.service.dto.PageableDto;
 import com.kolos.bookstore.service.exception.InvalidOrderStatusTransitionException;
 import com.kolos.bookstore.service.exception.NotFoundException;
+import com.kolos.bookstore.service.util.MessageManager;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,13 +27,16 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
     private final ServiceMapper serviceMapper;
+    private final MessageManager messageManager;
+    @PersistenceContext
+    private EntityManager manager;
 
 
     @Override
     public OrderDto get(Long id) {
         Order order = orderRepository.find(id);
         if (order == null) {
-            throw new NotFoundException(MassageManager.INSTANCE.getMessage("order.not_found") + id);
+            throw new NotFoundException(messageManager.getMessage("order.not_found") + id);
         }
         return serviceMapper.toDto(order);
     }
@@ -40,8 +46,8 @@ public class OrderServiceImpl implements OrderService {
         List<OrderDto> orders = orderRepository.findAll(pageableDto.getLimit(), pageableDto.getOffset()).stream()
                 .map(serviceMapper::toDto)
                 .toList();
-        int count = orderRepository.countAll();
-        int pages = getTotalPages(pageableDto, count);
+        long count = orderRepository.countAll();
+        long pages = getTotalPages(pageableDto, count);
         pageableDto.setTotalItems(count);
         pageableDto.setTotalPages(pages);
         return orders;
@@ -49,67 +55,73 @@ public class OrderServiceImpl implements OrderService {
 
 
     @Override
+    @Transactional
     public OrderDto create(OrderDto dto) {
         Order order = serviceMapper.toEntity(dto);
+        order.setUser(manager.merge(order.getUser()));
         Order savedOrder = orderRepository.save(order);
         return serviceMapper.toDto(savedOrder);
     }
 
+    @Transactional
     @Override
-    public List<OrderDto> getOrdersByUserId(Long userId, PageableDto pageable) {
-        List<OrderDto> orders = orderRepository.findByUserId(userId, pageable.getLimit(), pageable.getOffset()).stream()
+    public List<OrderDto> getOrdersByUserId(Long id, PageableDto pageable) {
+        List<OrderDto> orders = orderRepository.findByUserId(id, pageable.getLimit(), pageable.getOffset()).stream()
                 .map(serviceMapper::toDto)
                 .toList();
-        int count = orderRepository.countAllMassage(userId);
-        int pages = getTotalPages(pageable, count);
+        long count = orderRepository.countAllById(id);
+        long pages = getTotalPages(pageable, count);
         pageable.setTotalItems(count);
         pageable.setTotalPages(pages);
         return orders;
     }
 
+    @Transactional
     @Override
     public void changeStatus(Long orderId, OrderStatusUpdateDto.Status dtoStatus) {
         Order order = orderRepository.find(orderId);
         validateChangeStatus(orderId, dtoStatus, order);
         order.setStatus(Order.Status.valueOf(dtoStatus.name()));
-        orderRepository.update(order);
+        orderRepository.save(order);
     }
 
 
     @Override
+    @Transactional
     public void cancelOrder(Long orderId) {
         Order order = orderRepository.find(orderId);
         if (order.getStatus().equals(Order.Status.CANCELLED)) {
-            throw new InvalidOrderStatusTransitionException(MassageManager.INSTANCE.getMessage("order.cannot_change_cancelled"));
+            throw new InvalidOrderStatusTransitionException(messageManager.getMessage("order.cannot_change_cancelled"));
         }
-        orderRepository.update(order);
+        order.setStatus(Order.Status.CANCELLED);
+        orderRepository.save(order);
     }
 
 
-    private static void validateChangeStatus(Long orderId, OrderStatusUpdateDto.Status dtoStatus, Order order) {
+    private  void validateChangeStatus(Long orderId, OrderStatusUpdateDto.Status dtoStatus, Order order) {
         if (order == null) {
-            throw new NotFoundException(MassageManager.INSTANCE.getMessage("order.not_found") + orderId);
+            throw new NotFoundException(messageManager.getMessage("order.not_found") + orderId);
         }
 
         if (order.getStatus().equals(Order.Status.CANCELLED)) {
-            throw new InvalidOrderStatusTransitionException(MassageManager.INSTANCE.getMessage("order.cannot_change_cancelled"));
+            throw new InvalidOrderStatusTransitionException(messageManager.getMessage("order.cannot_change_cancelled"));
         }
 
         if (order.getStatus().equals(Order.Status.PENDING)) {
             if (!dtoStatus.equals(OrderStatusUpdateDto.Status.CANCELLED) && !dtoStatus.equals(OrderStatusUpdateDto.Status.PAID)) {
-                throw new InvalidOrderStatusTransitionException(MassageManager.INSTANCE.getMessage("order.invalid_status_transition_pending"));
+                throw new InvalidOrderStatusTransitionException(messageManager.getMessage("order.invalid_status_transition_pending"));
             }
         }
 
         if (order.getStatus().equals(Order.Status.PAID)) {
             if (!dtoStatus.equals(OrderStatusUpdateDto.Status.DELIVERED) && !dtoStatus.equals(OrderStatusUpdateDto.Status.CANCELLED)) {
-                throw new InvalidOrderStatusTransitionException(MassageManager.INSTANCE.getMessage("order.invalid_status_transition_paid"));
+                throw new InvalidOrderStatusTransitionException(messageManager.getMessage("order.invalid_status_transition_paid"));
             }
         }
 
         if (order.getStatus().equals(Order.Status.DELIVERED)) {
             if (!dtoStatus.equals(OrderStatusUpdateDto.Status.CANCELLED)) {
-                throw new InvalidOrderStatusTransitionException(MassageManager.INSTANCE.getMessage("order.invalid_status_transition_delivered"));
+                throw new InvalidOrderStatusTransitionException(messageManager.getMessage("order.invalid_status_transition_delivered"));
             }
         }
     }
