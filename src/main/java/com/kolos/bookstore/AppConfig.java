43530d07c16293c8f5f406c86938aa551df62b5e
sanya <kolosovskii.201416@gmail.com>
2024-09-16 16:14:57 +0300

    removed the jdbcTemplate and nameParameterJdbcTemplate beans, and also added beans for working with entityManagerFactory and transactionManager.

--- src/main/java/com/kolos/bookstore/AppConfig.java
+++ src/main/java/com/kolos/bookstore/AppConfig.java
@@ -1,57 +1,28 @@
 package com.kolos.bookstore;
 
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-
-import javax.sql.DataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @ComponentScan
-@PropertySource("classpath:application.properties")
+@EnableTransactionManagement
 public class AppConfig {
 
-    @Value("${db.url}")
-    private String url;
-
-    @Value("${db.user}")
-    private String username;
-
-    @Value("${db.password}")
-    private String password;
-
-    @Value("${db.driver}")
-    private String driver;
-
-    @Value("${db.poolSize}")
-    private int poolSize;
 
     @Bean
-    public DataSource dataSource() {
-        HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        dataSource.setDriverClassName(driver);
-        dataSource.setMaximumPoolSize(poolSize);
-        return dataSource;
-    }
-
-
-    @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-        return new JdbcTemplate(dataSource);
+    public EntityManagerFactory entityManagerFactory() {
+        return Persistence.createEntityManagerFactory("psql");
     }
 
     @Bean
-    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
-        return new NamedParameterJdbcTemplate(dataSource);
+    public TransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
     }
-
 }
