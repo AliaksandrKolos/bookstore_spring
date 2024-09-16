0e4ae02087219a1d5d3e7c1249f7e4e5c6a02713
sanya <kolosovskii.201416@gmail.com>
2024-09-16 16:19:10 +0300

    Refactor Book entity to use JPA annotations and add additional fields

--- src/main/java/com/kolos/bookstore/data/entity/Book.java
+++ src/main/java/com/kolos/bookstore/data/entity/Book.java
@@ -1,19 +1,49 @@
 package com.kolos.bookstore.data.entity;
 
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "author", nullable = false)
     private String author;
+
+    @Column(name = "isbn")
     private String isbn;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "genre")
     private String genre;
+
+    @Column(name = "year")
     private Integer year;
+
+    @Column(name = "deleted")
+    private boolean deleted;
+
+    @Column(name = "pages")
     private Integer pages;
+
+    @Column(name = "price")
     private BigDecimal price;
+
+    @Column(name = "cover_id")
     private Cover cover;
 
     public enum Cover {
@@ -21,5 +51,21 @@ public class Book {
         HARDCOVER,
         LEATHER,
         DUST_JACKET
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Book book = (Book) o;
+        return getId() != null && Objects.equals(getId(), book.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
