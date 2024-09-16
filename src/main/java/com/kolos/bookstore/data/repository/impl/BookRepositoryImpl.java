0a33806c21afda3f5c222d6b0fc0be2b7b9501fc
sanya <kolosovskii.201416@gmail.com>
2024-09-16 16:25:50 +0300

    Refactored BookRepositoryImpl to use JPA Criteria API and EntityManager for querying and transaction management.

--- src/main/java/com/kolos/bookstore/data/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/kolos/bookstore/data/repository/impl/BookRepositoryImpl.java
@@ -1,91 +1,133 @@
 package com.kolos.bookstore.data.repository.impl;
 
-import com.kolos.bookstore.data.dao.BookDao;
-import com.kolos.bookstore.data.dto.BookDto;
 import com.kolos.bookstore.data.entity.Book;
-import com.kolos.bookstore.data.mapper.DataMapper;
 import com.kolos.bookstore.data.repository.BookRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Transactional
 @Repository
 public class BookRepositoryImpl implements BookRepository {
 
-    private final BookDao bookDao;
-    private final DataMapper dataMapper;
+    @PersistenceContext
+    private EntityManager manager;
 
 
     @Override
-    public Book find(Long id) {
-        BookDto bookDto = bookDao.find(id);
-        return dataMapper.toEntity(bookDto);
+    public List<Book> findByAuthor(String author, long limit, long offset) {
+        CriteriaBuilder cb = manager.getCriteriaBuilder();
+
+        CriteriaQuery<Book> cq = cb.createQuery(Book.class);
+        Root<Book> root = cq.from(Book.class);
+        cq.where(
+                cb.and(
+                        cb.equal(root.get("author"), author),
+                        cb.equal(root.get("deleted"), false)
+
+                )
+        );
+        TypedQuery<Book> query = manager.createQuery(cq);
+        return query.setFirstResult((int) offset).setMaxResults((int) limit).getResultList();
     }
 
     @Override
-    public Book save(Book entity) {
-        BookDto bookDto = dataMapper.toDto(entity);
-        BookDto saved = bookDao.save(bookDto);
-        return dataMapper.toEntity(saved);
-    }
-
-    @Override
-    public Book update(Book entity) {
-        BookDto bookDto = dataMapper.toDto(entity);
-        bookDto.setId(entity.getId());
-        BookDto updated = bookDao.update(bookDto);
-        return dataMapper.toEntity(updated);
-    }
-
-    @Override
-    public Book findByIsbn(String isbn) {
-        BookDto byIsbn = bookDao.findByIsbn(isbn);
-        if (byIsbn == null) {
-            return null;
-        }
-        return dataMapper.toEntity(byIsbn);
-    }
-
-    @Override
-    public List<Book> findByAuthor(String author, int limit, int offset) {
-        return bookDao.findByAuthor(author, limit, offset)
-                .stream()
-                .map(dataMapper::toEntity)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public int countAll() {
-        return bookDao.countAll();
+    public long countAll() {
+        TypedQuery<Long> query = manager.createQuery("SELECT COUNT(*) FROM Book b WHERE b.deleted = false ", Long.class);
+        return query.getSingleResult();
     }
 
 
     @Override
     public int countAll(String messages) {
-        return bookDao.countAllSearch(messages);
+        CriteriaBuilder cb = manager.getCriteriaBuilder();
+
+        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+        Root<Book> root = cq.from(Book.class);
+
+        cq.select(cb.count(root)).where(
+                cb.and(
+                        cb.equal(root.get("deleted"), false),
+                        cb.like(root.get("title"), "%" + messages + "%")
+                )
+        );
+        TypedQuery<Long> query = manager.createQuery(cq);
+        return query.getSingleResult().intValue();
+    }
+
+
+    @Override
+    public List<Book> findAllByTitle(String title, long limit, long offset) {
+        CriteriaBuilder cb = manager.getCriteriaBuilder();
+        CriteriaQuery<Book> cq = cb.createQuery(Book.class);
+        Root<Book> root = cq.from(Book.class);
+        cq.where(
+                cb.and(
+                        cb.like(cb.lower(root.get("title")), "%" + title.toLowerCase() + "%"),
+                        cb.equal(root.get("deleted"), false)
+                )
+        );
+        TypedQuery<Book> query = manager.createQuery(cq);
+        return query.setFirstResult((int) offset).setMaxResults((int) limit).getResultList();
     }
 
     @Override
-    public List<Book> findAllSearch(String searchMessage, int limit, int offset) {
-        return bookDao.findAllSearch(searchMessage, offset, limit)
-                .stream()
-                .map(dataMapper::toShortEntity)
-                .collect(Collectors.toList());
+    public List<Book> findAll(long limit, long offset) {
+        CriteriaBuilder cb = manager.getCriteriaBuilder();
+
+        CriteriaQuery<Book> cq = cb.createQuery(Book.class);
+        Root<Book> root = cq.from(Book.class);
+        cq.select(root).where(cb.equal(root.get("deleted"), false));
+        TypedQuery<Book> query = manager.createQuery(cq);
+        return query.setFirstResult((int) offset).setMaxResults((int) limit).getResultList();
     }
 
+
     @Override
-    public List<Book> findAll(int limit, int offset) {
-        return bookDao.findAll(limit, offset)
-                .stream()
-                .map(dataMapper::toShortEntity)
-                .collect(Collectors.toList());
+    public Book findByIsbn(String isbn) {
+        return manager.createQuery("FROM Book b WHERE b.isbn = :isbn AND b.deleted = false", Book.class)
+                .setParameter("isbn", isbn)
+                .getResultStream()
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public boolean delete(Long id) {
-        return bookDao.delete(id);
+        Book book = manager.find(Book.class, id);
+        if (book != null) {
+            book.setDeleted(true);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Book find(Long id) {
+        return manager.createQuery("FROM Book b WHERE b.id = :id AND b.deleted = false", Book.class)
+                .setParameter("id", id)
+                .getResultStream()
+                .findFirst()
+                .orElse(null);
+    }
+
+
+    @Override
+    public Book save(Book entity) {
+        if (entity.getId() == null) {
+            manager.persist(entity);
+        } else {
+            manager.merge(entity);
+        }
+        return entity;
     }
 }
