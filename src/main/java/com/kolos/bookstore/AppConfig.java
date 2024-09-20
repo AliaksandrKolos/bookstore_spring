00d1d3580e0a345afbbc05a7d24b99c8a2dc8624
sanya <kolosovskii.201416@gmail.com>
2024-09-20 11:36:10 +0300

    Implemented all controllers for working with the Login  using Spring Mvc

--- src/main/java/com/kolos/bookstore/AppConfig.java
+++ src/main/java/com/kolos/bookstore/AppConfig.java
@@ -1,19 +1,54 @@
 package com.kolos.bookstore;
 
-
+import com.kolos.bookstore.web.controller.interceptor.AuthenticationInterceptor;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @ComponentScan
 @EnableTransactionManagement
-public class AppConfig {
+@RequiredArgsConstructor
+public class AppConfig extends WebMvcConfigurationSupport {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/books/getAll", "/books/{id:\\d+}", "/login",
+                        "/users/registration", "/cart", "/books/addToCart",
+                        "/changeLanguage", "/books/search_title");
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**", "/images/**", "/js/**")
+                .addResourceLocations("classpath:/static/css/", "classpath:/static/images/", "classpath:/static/js/");
+    }
 
 
     @Bean
