48a78bf625000b7fb02bcd7f5c9ed53ec6a5b342
sanya <kolosovskii.201416@gmail.com>
2024-09-24 10:20:59 +0300

    Changed the BookRepository interface to work with SpringData

--- src/main/java/com/kolos/bookstore/data/repository/BookRepository.java
+++ src/main/java/com/kolos/bookstore/data/repository/BookRepository.java
@@ -1,20 +1,35 @@
 package com.kolos.bookstore.data.repository;
 
 import com.kolos.bookstore.data.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import java.util.Optional;
 
-public interface BookRepository extends AbstractRepository<Long, Book> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Book findByIsbn(String isbn);
+    @Query("FROM Book b WHERE b.isbn = :isbn AND b.deleted = false")
+    Optional<Book> findByIsbn(String isbn);
 
-    List<Book> findByAuthor(String author, long limit, long offset);
+    @Query("FROM Book b where b.author = :author and b.deleted = false")
+    Page<Book> findByAuthor(String author, Pageable pageable);
 
-    long countAll();
+    @Query("from Book b where b.title like %:searchMessage% and b.deleted = false")
+    Page<Book> findAllByTitle(String searchMessage, Pageable pageable);
 
-    List<Book> findAllByTitle(String searchMessage, long limit, long offset);
+    @Override
+    @Query("FROM Book b WHERE b.deleted = false")
+    Page<Book> findAll(Pageable pageable);
 
-    int countAll(String messages);
+    @Override
+    @Query("from Book b where b.id = :id and b.deleted = false")
+    Optional<Book> findById(Long id);
 
-
+    @Modifying
+    @Query("UPDATE Book b SET b.deleted = true WHERE b.id = :id")
+    void deleteById(Long id);
 }
+
