725662917f5b1529fa3c8a9c0cec020bd867a7e8
sanya <kolosovskii.201416@gmail.com>
2024-09-24 10:16:16 +0300

    Changed the UserRepository interface to work with SpringData

--- src/main/java/com/kolos/bookstore/data/repository/UserRepository.java
+++ src/main/java/com/kolos/bookstore/data/repository/UserRepository.java
@@ -1,15 +1,32 @@
 package com.kolos.bookstore.data.repository;
 
 import com.kolos.bookstore.data.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
+import java.util.Optional;
 
-public interface UserRepository extends AbstractRepository<Long, User> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
 
-    List<User> findByLastName(String lastName, long limit, long offset);
+    @Override
+    @Query("FROM User u WHERE u.id = :id AND u.deleted = false")
+    Optional<User> findById(Long id);
 
-    long countAll();
+    @Modifying
+    @Query("UPDATE User u SET u.deleted = true WHERE u.id = :id")
+    void deleteById(Long id);
 
+    @Query("FROM User u WHERE u.email = :email AND u.deleted = false")
+    Optional<User> findByEmail(String email);
+
+    @Query("FROM User u WHERE LOWER(u.lastName) = LOWER(:lastName) AND u.deleted = false")
+    Page<User> findAllByLastName(String lastName, Pageable pageable);
+
+    @Override
+    @Query("FROM User u WHERE u.deleted = false")
+    Page<User> findAll(Pageable pageable);
 }
