98345a58fede69f78f8256675850bfd6465253c4
sanya <kolosovskii.201416@gmail.com>
2024-09-24 10:31:07 +0300

    Changed password hashing method from SHA-1 to BCrypt

--- src/main/java/com/kolos/bookstore/service/impl/DigestServiceImpl.java
+++ src/main/java/com/kolos/bookstore/service/impl/DigestServiceImpl.java
@@ -2,27 +2,19 @@ package com.kolos.bookstore.service.impl;
 
 
 import com.kolos.bookstore.service.DigestService;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
-
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 @Service
 public class DigestServiceImpl implements DigestService {
 
     @Override
     public String hash(String input) {
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
-            byte[] bytesIn = input.getBytes();
-            messageDigest.update(bytesIn);
-            byte[] bytesOut = messageDigest.digest();
-            BigInteger bigInt = new BigInteger(1, bytesOut);
-            String hash = bigInt.toString(16).toUpperCase();
-            return hash;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(input, salt);
+    }
+
+    public boolean verify(String input, String hashedPassword) {
+        return BCrypt.checkpw(input, hashedPassword);
     }
 }
