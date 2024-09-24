5b06b8fa1bc33b1e6a27e16d275195ff14eaec8e
sanya <kolosovskii.201416@gmail.com>
2024-09-24 10:22:58 +0300

    Changed the OrderRepository interface to work with SpringData

--- src/main/java/com/kolos/bookstore/data/repository/OrderRepository.java
+++ src/main/java/com/kolos/bookstore/data/repository/OrderRepository.java
@@ -1,15 +1,12 @@
 package com.kolos.bookstore.data.repository;
 
 import com.kolos.bookstore.data.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
-public interface OrderRepository extends AbstractRepository<Long, Order> {
-
-    List<Order> findByUserId(Long id, long limit, long offset);
-
-    long countAll();
-
-    long countAllById(Long id);
+    Page<Order> findByUserId(Long id, Pageable pageable);
 
 }
