ea8cac4fcabf761ea6776159bf85e142ac115c4f
sanya <kolosovskii.201416@gmail.com>
2024-09-30 21:13:23 +0300

    Added validation to dto objects

--- src/main/java/com/kolos/bookstore/service/dto/UserDto.java
+++ src/main/java/com/kolos/bookstore/service/dto/UserDto.java
@@ -1,6 +1,10 @@
 package com.kolos.bookstore.service.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UserDto {
@@ -8,12 +12,15 @@ public class UserDto {
     private Long id;
     private String firstName;
     private String lastName;
+    @NotBlank
+    @Length(min = 5, max = 50, message = "min length 6 simbl, max length 50")
     private String password;
+    @NotNull(message = "Should be not null")
+    @Email(message = "Email is not valid")
     private String email;
     private Role role;
 
     public enum Role {
         USER, MANAGER, ADMIN
     }
-
 }
