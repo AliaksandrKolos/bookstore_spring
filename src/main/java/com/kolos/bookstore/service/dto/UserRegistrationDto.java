4e20abc05a9c1c6f7fa5d55fcbd00e2aa2ed9a48
sanya <kolosovskii.201416@gmail.com>
2024-10-02 11:47:21 +0300

    added validation at the dto level in the UserRegistrationDto class

--- src/main/java/com/kolos/bookstore/service/dto/UserRegistrationDto.java
+++ src/main/java/com/kolos/bookstore/service/dto/UserRegistrationDto.java
@@ -1,10 +1,14 @@
 package com.kolos.bookstore.service.dto;
 
+import jakarta.validation.constraints.Email;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UserRegistrationDto {
 
+    @Length(min = 3, max = 20, message = "min length 3 max length 20")
     private String password;
+    @Email(message = "No valid email")
     private String email;
 }
